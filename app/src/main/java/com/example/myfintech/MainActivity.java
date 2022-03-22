760c68d2fdad8217f345b1e8ad6823306b980651
DomantasKarasa <Domantask13@gmail.com>
2022-03-22 12:42:33 +0200

    Added button for text change

--- app/src/main/java/com/example/myfintech/MainActivity.java
+++ app/src/main/java/com/example/myfintech/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.myfintech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ChangeBtn;
+    TextView TextOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ChangeBtn = findViewById(R.id.ChangeText);
+        TextOK = findViewById(R.id.TextResult);
+        ChangeBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Time now = new Time();
+                now.setToNow();
+
+                if (now.hour < 12) {
+                    TextOK.setText("Labas rytas");
+                }
+
+                if (now.hour >= 12 && now.hour < 18) {
+                    TextOK.setText("Laba diena");
+                }
+
+                if (now.hour >= 18) {
+                    TextOK.setText("Labas vakaras");
+                }
+            }
+        });
     }
 }
